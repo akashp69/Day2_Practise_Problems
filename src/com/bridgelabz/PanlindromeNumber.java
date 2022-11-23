e761c64083f5372f93825a70897ef3af2330c8b1
akashp69 <akashpakhare499@gmail.com>
2022-11-23 09:40:03 +0530

    Added:Program to Find Palindrome Number

--- src/com/bridgelabz/PanlindromeNumber.java
+++ src/com/bridgelabz/PanlindromeNumber.java
@@ -4,14 +4,14 @@ public class PanlindromeNumber {
     public static void main(String[] args) {
 
 
-        int num = 343, p = 0;
-        for (; num != 0; ) {
-            int remainder = num % 10;
-            p = p * 10 + remainder;
-            num = num / 10;
+        int number = 343, palindrome = 0;
+        for (; number != 0; ) {
+            int remainder = number % 10;
+            palindrome = palindrome * 10 + remainder;
+            number = number / 10;
         }
 
-        System.out.println("The given number is Palindrom: " + p);
+        System.out.println("The given number is Palindrom: " + palindrome);
 
     }
 }
