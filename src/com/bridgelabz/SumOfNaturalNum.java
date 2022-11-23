35f587f36911cdfb22b1bcc182e5ae77955137bb
akashp69 <akashpakhare499@gmail.com>
2022-11-23 09:18:56 +0530

    Added:Program For Sum Of Given Naturals Numbers

--- src/com/bridgelabz/SumOfNaturalNum.java
+++ src/com/bridgelabz/SumOfNaturalNum.java
@@ -1,10 +1,10 @@
 package com.bridgelabz;
 
-public class SumOfNaturalNum {
+public class SumOfNaturalNumber {
     static void checkSum(){
-        int n=5,i=1,sum=0;
+        int number=5,integer=1,sum=0;
 
-        while (i<=n ){
+        while (integer<=number ){
             sum=sum+i;
             i++;
         }
@@ -12,8 +12,6 @@ public class SumOfNaturalNum {
     }
 
     public static void main(String[] args) {
-        System.out.println("Enter a Digit For Sum Of Number");
         checkSum();
-
     }
 }
