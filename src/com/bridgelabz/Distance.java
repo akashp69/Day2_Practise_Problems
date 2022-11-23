9de2fcbc3266722740811f4b2bd81cf89c15e693
akashp69 <akashpakhare499@gmail.com>
2022-11-23 09:47:34 +0530

    Added:Program For Calculate The Distance Of Two Points

--- src/com/bridgelabz/Distance.java
+++ src/com/bridgelabz/Distance.java
@@ -7,9 +7,9 @@ public class Distance {
         double x1;
         double y1;
         System.out.println("Enter the coordinates:x1,y1");
-        Scanner s  =new Scanner(System.in);
-        x1 = s.nextDouble();
-        y1 = s.nextDouble();
+        Scanner scanner  =new Scanner(System.in);
+        x1 = scanner.nextDouble();
+        y1 = scanner.nextDouble();
         double distance = Math.sqrt((Math.pow(x1, 2) + (Math.pow(y1, 2))));
         System.out.println("Eucledian distance between entered coordinates is = "+distance);
     }
