cecc4725deb49f44edf0e3df16d11f96b872b50b
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 01:39:04 +0530

    Update PanlindromeNumber.java

--- src/com/bridgelabz/PanlindromeNumber.java
+++ src/com/bridgelabz/PanlindromeNumber.java
@@ -1,10 +1,16 @@
 package com.bridgelabz;
 
 public class PanlindromeNumber {
+    /**
+    *This Method Created For Check The Number Is Palindrome 
+    */
     public static void main(String[] args) {
 
 
         int number = 343, palindrome = 0;
+    /**
+    *Using For check Palindrome Number
+    */
         for (; number != 0; ) {
             int remainder = number % 10;
             palindrome = palindrome * 10 + remainder;
