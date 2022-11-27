23827af3afd336dae46e9a58d49bd7a857343f17
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 01:47:48 +0530

    Update SumOfNaturalNum.java

--- src/com/bridgelabz/SumOfNaturalNum.java
+++ src/com/bridgelabz/SumOfNaturalNum.java
@@ -1,15 +1,28 @@
 package com.bridgelabz;
 
+
 public class SumOfNaturalNumber {
+    /**
+    *This Static Method Created For Sum Of Natural Numbers
+    */
     static void checkSum(){
         int number=5,integer=1,sum=0;
+    /**
+    *Using While Loop For Sum Of Naturals  Numbers 
+    */
 
         while (integer<=number ){
             sum=sum+i;
             i++;
         }
+    /**
+    *Print The sum Of Naturals Numbers
+    */
         System.out.println(sum);
     }
+    /**
+    *This Method Created For Calling The Static Method checkSum
+    */
 
     public static void main(String[] args) {
         checkSum();
