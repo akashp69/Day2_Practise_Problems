0aad0e6ae13bdfc36290e65b405f1b39f0acbfb7
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 01:22:48 +0530

    Update DisplayNumUnit.java

--- src/com/bridgelabz/DisplayNumUnit.java
+++ src/com/bridgelabz/DisplayNumUnit.java
@@ -3,11 +3,17 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class DisplayNumUnit {
+    /**
+    *This Method Created For Checking The Unit Of Numbers 1,10,100,1000
+    */
 
     static void checkUnit() {
         int number;
         Scanner scanner = new Scanner(System.in);
         number = scanner.nextInt();
+    /**
+    *Using If-else-if loop  For Printing The Unit Of Given Number
+    */
         if (number >=  1 && number<1001) {
             if (number == 1)
                 System.out.println("ONE");
@@ -24,6 +30,9 @@ public class DisplayNumUnit {
 
 
     }
+    /**
+    *This Method Created For call the checkUnit Method 
+    */
 
     public static void main(String[] args) {
         System.out.println("Enter Number 1,10,100,1000 For Checking Unit");
