9de861badda4857ed1485cd5fc107b0b8f4af434
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 01:42:25 +0530

    Update ReverseNumber.java

--- src/com/bridgelabz/ReverseNumber.java
+++ src/com/bridgelabz/ReverseNumber.java
@@ -1,17 +1,29 @@
 package com.bridgelabz;
 
 public class ReverseNumber {
+    /**
+    *This Static Method Created For  the Given Number Print in  Reverse
+    */
     static void checkReverse(){
         int number = 143,reverse=0;
         System.out.println(n);
+    /**
+    *Using While Loop For Reversing the Given Number
+    */
         while (n>0){
             int remainder=n%10;
             reverse =reverse*10+remainder;
             number=number/10;
         }
+     /**
+    *Print Number in Reverse Order
+    */
         System.out.println(reverse);
 
     }
+    /**
+    *This Method Created For Calling the Static Method checkReverse
+    */
 
     public static void main(String[] args) {
 
